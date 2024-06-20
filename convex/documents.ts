6cd0fc3bdd4b4db99e75f297cefe50750d67ca4a
Shubham Khade <shubhkdesign@gmail.com>
2024-06-20 12:13:52 +0530

    scoping data for individual user

---
+++ convex/documents.ts
@@ -0,0 +1,35 @@
+import { error } from "console";
+import { mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+
+export const getDocuments = query({
+  async handler(ctx, args) {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    if (!userId) {
+      return [];
+    }
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+      .collect();
+  },
+});
+
+export const createDocument = mutation({
+  args: {
+    title: v.string(),
+  },
+  async handler(ctx, args) {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    if (!userId) {
+      throw new ConvexError("Not authenticated");
+    }
+
+    await ctx.db.insert("documents", {
+      title: args.title,
+      tokenIdentifier: userId,
+    });
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 import Image from "next/image";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments, {});
+  const createDocument = useMutation(api.documents.createDocument);
+
   return (
     <main className="flex pt-10 pr-10 min-h-screen flex-col items-end justify-start">
       <Unauthenticated>
@@ -11,6 +20,14 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button onClick={() => createDocument({ title: "Testing mutations" })}>
+          CLICK ME
+        </button>
+        {documents?.map((document) => (
+          <ul key={document._id}>
+            <li>{document.title}</li>
+          </ul>
+        ))}
       </Authenticated>
     </main>
   );
